15ca19949d3cef490e3e2cbad92611945ecbee86
Maria Eduarda <maria.correa.lima24@escola.pr.gov.br>
2026-05-06 09:35:40 -0300

    .

--- main.js
+++ main.js
@@ -13,12 +13,12 @@ for (let i = 0; i < botoes.length; i++) {
   };
 }
 
-// ⚠️ Datas FUTURAS
+// Datas ATUALIZADAS conforme solicitado
 const tempos = [
-  new Date("2026-12-31T00:00:00"),
-  new Date("2026-11-30T00:00:00"),
-  new Date("2026-10-30T00:00:00"),
-  new Date("2026-09-30T00:00:00")
+  new Date("2026-05-07T00:00:00"), // Primeira data: 07/05/2026
+  new Date("2026-07-20T00:00:00"), // Segunda data: 20/07/2026
+  new Date("2026-10-05T00:00:00"), // Terceira data: 05/10/2026
+  new Date("2026-12-20T00:00:00")  // Quarta data: 20/12/2026
 ];
 
 function calculaTempo(tempoObjetivo) {
